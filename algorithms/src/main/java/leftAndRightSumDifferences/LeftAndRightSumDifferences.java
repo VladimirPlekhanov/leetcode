c569fee60f8f823cc814b2b1bca0485634f5eede
Vladimir Plekhanov <vladimir_plekhanov@epam.com>
2023-05-05 14:53:52 +0400

    add solved "Left and Right Sum Differences" problem

--- algorithms/src/main/java/leftAndRightSumDifferences/LeftAndRightSumDifferences.java
+++ algorithms/src/main/java/leftAndRightSumDifferences/LeftAndRightSumDifferences.java
@@ -2,19 +2,21 @@ package leftAndRightSumDifferences;
 
 public class LeftAndRightSumDifferences {
 
+    // Quadratic complexity - O(n^2)
     public int[] leftRightDifference(int[] nums) {
         int length = nums.length;
-        if (length == 1) return new int[]{0};
-        // {2, 4, 7}
-        else {
-            int[] leftArr = new int[length];
-            int[] rightArr = new int[length];
+        int[] leftArr = new int[length];
+        int[] rightArr = new int[length];
+
+        if (length == 1) {
+            return new int[]{0};
+        } else {
             for (int i = 0; i < length; i++) {
                 if (i == 0) {
                     leftArr[i] = 0;
                 } else {
                     for (int j = 0; j < i; j++) {
-                        leftArr[i] += nums[j]; //i = 1 ok, i = 2
+                        leftArr[i] += nums[j];
                     }
                 }
             }
@@ -22,14 +24,37 @@ public class LeftAndRightSumDifferences {
                 if (i == length - 1) {
                     rightArr[i] = 0;
                 } else {
-                    for (int j = i; j > -1; j--) {
-                        rightArr[i] += nums[j]; //i = 1 ok, i = 0
+                    for (int j = i + 1; j < length; j++) {
+                        rightArr[i] += nums[j];
                     }
                 }
             }
         }
 
-        return new int[]{length};
+        int[] result = new int[length];
+        for (int i = 0; i < length; i++) {
+            result[i] = Math.abs(leftArr[i] - rightArr[i]);
+        }
+
+        return result;
+    }
+
+    // Logarithmic complexity - O(log n)
+    public int[] leftRigthDifference(int[] nums) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        int[] ans = new int[nums.length];
+        int leftsum = 0;
+        int rightsum = sum - nums[0];
+        for (int i = 0; i < nums.length - 1; i++) {
+            ans[i] = Math.abs(leftsum - rightsum);
+            leftsum += nums[i];
+            rightsum -= nums[i + 1];
+        }
+        ans[nums.length - 1] = Math.abs(leftsum - rightsum);
+        return ans;
     }
 
 }
--- algorithms/src/main/java/leftAndRightSumDifferences/LeftAndRightSumDifferencesTest.java
+++ algorithms/src/main/java/leftAndRightSumDifferences/LeftAndRightSumDifferencesTest.java
@@ -1,7 +1,24 @@
 package leftAndRightSumDifferences;
 
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Test;
+
 public class LeftAndRightSumDifferencesTest {
 
+    LeftAndRightSumDifferences l = new LeftAndRightSumDifferences();
+    int[] array1 = new int[]{10,4,8,3};
+    int[] array2 = new int[]{8, 28, 35, 21, 13, 21, 72, 35, 52, 74};
 
+
+    @Test
+    public void leftAndRightSumDifferencesTest() {
+        int[] expectedResult1 = new int[]{15,1,11,22};
+        int[] expectedResult2 = new int[]{351, 315, 252, 196, 162, 128, 35, 72, 159, 285};
+        int[] actualResult1 = l.leftRigthDifference(array1);
+        int[] actualResult2 = l.leftRigthDifference(array2);
+        assertArrayEquals(expectedResult1, actualResult1);
+        assertArrayEquals(expectedResult2, actualResult2);
+    }
 
 }
